4a84aefc2177f26d7274f76feb19e538e1500c0b
OuSebs <poolhernandezflores@gmail.com>
2026-05-08 17:24:49 -0500

    Primer

--- leccion.java
+++ leccion.java
@@ -11,7 +11,9 @@ public static void main(String[] args) {
 
     var op = largo*ancho;
 
-    System.out.println("El area del rectangulo es igual a "+op); 
+    System.out.println("El area del rectangulo es igual a "+op);
+    
+    //Hecho
 
 
 
