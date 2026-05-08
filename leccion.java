99616aee8e04f8ef3b23f581df2e08ed6e0ec1b3
OuSebs <poolhernandezflores@gmail.com>
2026-05-08 17:10:11 -0500

    Ejercicio1

---
+++ leccion.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class leccion {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Ingrese el largo: ");
+    var largo = sc.nextInt();
+    System.out.println("Ingrese el ancho: ");
+    var ancho = sc.nextInt();
+
+    var op = largo*ancho;
+
+    System.out.println("El area del rectangulo es igual a "+op);
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+}
